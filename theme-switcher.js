dcb177c376d96f4a28367543088da86b24cc3c63
初五 <141227996+ChuwuYo@users.noreply.github.com>
2025-05-17 12:33:50 +0800

    v1.6.5
    通过引入一个对象来管理不同主题下的按钮细节，减少 applyTheme 函数中的重复逻辑

--- theme-switcher.js
+++ theme-switcher.js
@@ -4,22 +4,32 @@ document.addEventListener('DOMContentLoaded', () => {
   const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
   const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
 
+  // 定义主题按钮的细节（图标和状态）
+  const themeButtonDetails = {
+    'dark': {
+      icon: '<img src="images/sun.svg" alt="Light Mode" width="16" height="16">', // 浅色模式图标
+      state: 'on'
+    },
+    'light': {
+      icon: '<img src="images/moon.svg" alt="Dark Mode" width="16" height="16">', // 深色模式图标
+      state: 'off'
+    }
+  };
+
   // 主题切换应用函数
   function applyTheme(theme) {
-    if (theme === 'dark') {
-      document.documentElement.setAttribute('data-bs-theme', 'dark');
-      if (themeToggleBtn) {
-        themeToggleBtn.innerHTML = '<img src="images/sun.svg" alt="Light Mode" width="16" height="16">'; // 浅色模式图标
-        themeToggleBtn.setAttribute('data-state', 'on');
+    // 设置文档根元素的主题属性
+    document.documentElement.setAttribute('data-bs-theme', theme);
+    // 将当前主题存储到本地存储
+    localStorage.setItem('theme', theme);
+
+    // 如果主题切换按钮存在，更新其内容和状态
+    if (themeToggleBtn) {
+      const details = themeButtonDetails[theme];
+      if (details) {
+        themeToggleBtn.innerHTML = details.icon;
+        themeToggleBtn.setAttribute('data-state', details.state);
       }
-      localStorage.setItem('theme', 'dark');
-    } else {
-      document.documentElement.setAttribute('data-bs-theme', 'light');
-      if (themeToggleBtn) {
-        themeToggleBtn.innerHTML = '<img src="images/moon.svg" alt="Dark Mode" width="16" height="16">'; // 深色模式图标
-        themeToggleBtn.setAttribute('data-state', 'off');
-      }
-      localStorage.setItem('theme', 'light');
     }
   }
 
@@ -35,7 +45,9 @@ document.addEventListener('DOMContentLoaded', () => {
   // 主题切换按钮监听器
   if (themeToggleBtn) {
     themeToggleBtn.addEventListener('click', () => {
+      // 根据当前主题确定新主题
       let newTheme = document.documentElement.getAttribute('data-bs-theme') === 'dark' ? 'light' : 'dark';
+      // 应用新主题
       applyTheme(newTheme);
     });
   }
